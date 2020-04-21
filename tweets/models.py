a0329261e0a4abef54ea489947b222225116023c
Sina Ebrahimi <ebr.sina@outlook.com>
2020-04-21 20:30:52 +0430

    setting and first app

---
+++ tweets/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class tweets(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.TextField(null=True,blank=True)
+    image  = models.FileField(upload_to='img/', null=True,blank=True)
